3cdaf469de4a0e218c66dcd18463195457da97b3
Eshaan Pareek <eshaanpareek@gmail.com>
2026-04-03 16:09:06 +0530

    Added llir-to-slim utility to convert LLVM IR to slim IR

---
+++ slim/main.cpp
@@ -0,0 +1,27 @@
+#include "llvm/IRReader/IRReader.h" // For parseIRFile
+#include "llvm/IR/LLVMContext.h"
+
+#include "slim/IR.h"
+
+using namespace llvm;
+
+LLVMContext context;
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2 || argc > 2)
+    {
+        llvm::errs() << "We expect exactly one argument - the name of the LLVM IR file!\n";
+        exit(1);
+    }
+
+    llvm::SMDiagnostic smDiagnostic;
+
+    std::unique_ptr<llvm::Module> module = parseIRFile(argv[1], smDiagnostic, context);
+
+    slim::IR *transformIR = new slim::IR(module);
+
+    transformIR->dumpIR();
+
+    return 0;
+}
